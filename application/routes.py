206fe828dd937c44ae35f2818385cb63897f188e
TomLLew <thomasllewelyn.tl@gmail.com>
2020-01-09 01:08:37 +0000

    trying to get form data working

--- application/routes.py
+++ application/routes.py
@@ -1,17 +1,20 @@
 from flask import render_template, request, redirect, url_for
 from application import frontend
+from application.forms import GenderForm
 import requests, json
 
 
 @frontend.route('/')
 @frontend.route('/home', methods=['GET', 'POST'])
 def home():
-    form = request.form.get('gender')
-    gender = {"gender":"none"}
-    gender["gender"] = form
-    stats = {}
-    if request.method == 'POST':
-        response = requests.post('http://127.0.0.1:5003/stats', params=gender).json()
+    form = GenderForm()
+    print(form.gender)
+    if form.validate_on_submit():
+        payload = request.form['gender']
+        print(form.gender)
+        stats = {}
+        
+        response = requests.post('http://127.0.0.1:5003/stats', params=payload).json()
         stats.update(response)
-        return response
-    return render_template('home.html', title='Dwarven Generator')
+        return render_template('home.html', title='Dwarven Generator', form=form)
+    return render_template('home.html', title='Dwarven Generator', form=form)
