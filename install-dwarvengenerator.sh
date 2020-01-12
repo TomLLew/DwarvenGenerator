fb0e6aace0a23499dbc29dfbc15e33d0d71b931f
TomLLew <thomasllewelyn.tl@gmail.com>
2020-01-12 17:40:30 +0000

    changed install

--- install-dwarvengenerator.sh
+++ install-dwarvengenerator.sh
@@ -2,9 +2,11 @@
 
 sudo apt-get -y update 
 sudo apt-get install git -y
+sleep 5
 sudo apt-get install python3 -y
 sudo apt-get install python3-pip -y
 sudo curl https://get.docker.com | sudo bash
+sleep 10
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker $(whoami)
